59bbd3cd789c7d65867fdb83fb92085587bcbb1e
Luiz Henrique de Menezes Carvalho <109999026+Luiz-Henrique-de-Menezes-Carvalho@users.noreply.github.com>
2022-09-01 17:29:20 -0300

    Update Estudante.java

--- RU/src/Estudante.java
+++ RU/src/Estudante.java
@@ -1,27 +1,26 @@
 import java.time.LocalDate;
 
 public class Estudante extends Usuario{
+    
+    public String matricula;
 
-    private int refeicoesDisponiveis; /*refeições disponíveis armazenaria a quantidade de refeições que o estudante comprou.
-    Porém, temos que ver como seria essa interação com o almoço e jantar*/
-
-    public Estudante(String codigo, String nome, String cpf, LocalDate dataDeNascimento, String email, String senha, int refeicoesDisponiveis) {
+    public Estudante(String codigo, String nome, String cpf, LocalDate dataDeNascimento, String email, String senha) {
         super(codigo, nome, cpf, dataDeNascimento, email, senha);
-        this.refeicoesDisponiveis = refeicoesDisponiveis;
+        
     }
+    
+    public String getMatricula() {
+		return matricula;
+	}
 
-    public int getRefeicoesDisponiveis() {
-        return refeicoesDisponiveis;
-    }
-
-    public void setRefeicoesDisponiveis(int refeicoesDisponiveis) {
-        this.refeicoesDisponiveis = refeicoesDisponiveis;
-    }
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
 
     @Override
     public String toString() {
         return String.format("Estudante: %s\nCPF: %s\nData de nascimento: %s\nE-mail: %s\nCódigo: %s\nQuantidade de refeições disponíveis: %d", super.getNome(),
-                                super.getCpf(), super.getDataDeNascimento(), super.getEmail(), super.getCodigo(), refeicoesDisponiveis);
+                                super.getCpf(), super.getDataDeNascimento(), super.getEmail(), super.getCodigo() );
     }
 
     
